c263b1f5a63e2cc9b0cd3cd86bd47bdc8936294a
Stenio2003 <steniodejesusnevesdebarros@gmail.com>
2026-05-21 11:17:55 +0100

    /Add de db

---
+++ apps/contratos/signal.py
@@ -0,0 +1,33 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from apps.contratos.models import Contrato
+from apps.notificacoes.models import Notificacao
+from apps.usuarios.models import Usuario
+
+
+@receiver(post_save, sender=Contrato)
+def criar_notificacao_admins(sender, instance, created, **kwargs):
+
+    if created:
+
+        admins = Usuario.objects.filter(
+            perfil=Usuario.PerfilChoices.ADMIN,
+            is_deleted=False,
+            status=Usuario.StatusChoices.ACTIVO
+        )
+
+        notificacoes = []
+
+        for admin in admins:
+
+            notificacoes.append(
+                Notificacao(
+                    utilizador=admin,
+                    tipo="sistema",
+                    titulo="Novo contrato",
+                    mensagem=f"O contrato {instance.titulo} na empresa {instance.cliente.empresa.nome}  pelo cliente {instance.cliente.nome}.",
+                )
+            )
+
+        Notificacao.objects.bulk_create(notificacoes)
